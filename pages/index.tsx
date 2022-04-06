b4539feabf09dd0653f3ee522515474e971654fa
张欣 <zhang.xin1@outlook.com>
2022-04-07 00:46:31 +0800

    修改主页

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,11 +19,6 @@ const Home: React.FC = () => {
           <br/>
           <span className='text-emerald-500'>&</span> web design.
         </h1>
-        <p className='mb-8 text-xl text-gray-900 text-opacity-60 text-center leading-relaxed'>
-          {/* 一些有关于网页开发、UI设计的内容
-          <br/>
-          正在这里发生 */}
-        </p>
         <div className='flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4'>
           <div className='button border border-gray-900 text-white bg-gray-800 hover:text-gray-900 hover:bg-gray-200' onClick={() => router.push('/posts')}>
             <IconBlog className='w-6 h-6 fill-current' />
