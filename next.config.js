7985599213dd3ee432b4bdee3c89f3081cad4285
张欣 <zhang.xin1@outlook.com>
2022-04-07 00:21:36 +0800

    静态资源托管

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const { withKeystone } = require("@keystone-6/core/next")
 
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = withKeystone({
+  env: {
+    STATIC_URL: isProd ? process.env.STATIC_URL : "",
+  },
+  assetPrefix: isProd ? process.env.STATIC_URL : "",
   reactStrictMode: true,
   webpack(config) {
     config.module.rules.push({
